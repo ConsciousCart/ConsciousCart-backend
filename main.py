22005daf76a989319642d4cce126793259413726
Jaya Manasa <jayamanasa.y@gmail.com>
2024-10-02 18:56:56 +0530

    removed slash from the link

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:3000",
-    "https://hackathon-project-frontend.vercel.app/"
+    "https://hackathon-project-frontend.vercel.app"
 ]
 
 app.add_middleware(
